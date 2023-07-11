bfee808372b580904208eda94d8a87f8be5b9ea1
Travis <travis3214@yahoo.com>
2023-07-11 14:48:10 -0400

    added starter files necessary

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// 3 global variables
+// add your api key 
+// 1 base for url
+// search history array
+
+// Function to display search history
+// Loop over jistory array and cound down so the most recent search is at the top
+// Function to update the search history in local storage
+// Function to get the seach history from the local storage
+// Function to display curretn weather data fromm the fetch request
+// Function to diplay the 5 day forecast from fetch request {needs icon, temp, humidity, date, wind speed}
+// Create elements for each of the above
+// Function to display 5 day forecast data from fetch request each card
+// Function specifically for geolocation with lat and lon and use function above to make calls
+// Function to add event listener
+// Dont forget to link the script
