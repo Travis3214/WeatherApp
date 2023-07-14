f4d044339b8c7d5955cbbc18c93856d6e07f5fc8
Travis <travis3214@yahoo.com>
2023-07-14 01:28:09 -0400

    added day js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,45 @@
-// 3 global variables
-// add your api key 
-// 1 base for url
-// search history array
-
-// Function to display search history
-// Loop over history array and cound down so the most recent search is at the top
-// Function to update the search history in local storage
-// Function to get the seach history from the local storage
-// Function to display curretn weather data fromm the fetch request
-// Function to diplay the 5 day forecast from fetch request {needs icon, temp, humidity, date, wind speed}
-// Create elements for each of the above
-// Function to display 5 day forecast data from fetch request each card
-// Function specifically for geolocation with lat and lon and use function above to make calls
-// Function to add event listener
-// Dont forget to link the script
-
 const apiKey = "5c5462b61afe403ec7f8a8d33ac004fc";
 
 const baseUrl = "https://openweathermap.org/";
 
 const searchHistory = [];
 
+// Function to display search history
+// Loop over history array and cound down so the most recent search is at the top
+
+
+
+// Function to update the search history in local storage
+
+
+
+
+// Function to get the seach history from the local storage
+
+
+
+// Function to display current weather data fromm the fetch request
+
+
+
+// Function to diplay the 5 day forecast from fetch request
+// needs icon, temp, humidity, date, wind speed
+
+
+
+// Create elements for each of the above to hold the data
+
+
+
+
+// Function to display 5 day forecast data from fetch request for each card
+
+
+
+
+// Function specifically for geolocation with lat and lon and use function above to make calls
+
+
+
+
+// Function to add event listener for search button
